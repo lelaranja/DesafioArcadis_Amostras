926aea6aace9c6ee03d28b8923f3d4ed11f3fd8b
lelaranja <leticiajlaranja@gmail.com>
2022-08-12 13:59:17 -0300

    fix: ajuste no db

--- Backend/src/database/create-and-populate.js
+++ Backend/src/database/create-and-populate.js
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS "PONTOS" (
     "NomeParametro" varchar(64),
     "CoordX" int,
     "CoordY" int,
-    "ValorAmostrado" int,
+    "ValorAmostrado" varchar(10),
     "UnidadeMedida" varchar(10),
     "DataColeta" varchar (64)
 );`;
@@ -17,9 +17,9 @@ CREATE TABLE IF NOT EXISTS "PONTOS" (
 const ADD_PONTOS_DATA = `
 INSERT INTO PONTOS (NomePonto, NomeParametro, CoordX, CoordY, ValorAmostrado, UnidadeMedida, DataColeta)
 VALUES 
-    ('PV-01', 'DBO', 333287.02, 7394586.09, 6,'mg O2/l', '2022-07-01'),
-    ('PA-05', 'Alumínio Dissolvido', 342593.07, 7380989.23, 0.1, 'mg/l', '2022-05-02'),
-    ('P-20', 'Chumbo Total', 687394.84, 7465628.92, 0.01, 'mg/l', '2022-06-10')
+    ('PV-01', 'DBO', 333287.02, 7394586.09, '6','mg O2/l', '2022-07-01'),
+    ('PA-05', 'Alumínio Dissolvido', 342593.07, 7380989.23, '0.1', 'mg/l', '2022-05-02'),
+    ('P-20', 'Chumbo Total', 687394.84, 7465628.92, '0.01', 'mg/l', '2022-06-10')
 `
 
 function criaTabelaPts() {
